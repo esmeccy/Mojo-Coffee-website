80351ef2b24eb8356b7a3e3841618f5c4cf11b44
esmeccy <esmec1004@gmail.com>
2026-08-05 11:11:58 -0700

    add GitHub Pages package

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(async ({ command }) => {
-  const plugins = [react()];
+  const plugins = [react()]
+
   if (command === 'serve') {
     try {
-      const iris = (await import('vite-plugin-iris')).default;
-      plugins.unshift(iris());
+      const iris = (await import('vite-plugin-iris')).default
+      plugins.unshift(iris())
     } catch {}
   }
 
-  return { plugins };
-});
+  return {
+    base: '/Mojo-Coffee-website/',
+    plugins,
+  }
+})
