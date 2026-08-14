c8f65f7733ffece34846eda69ceae24e4b9407f6
esmeccy <esmec1004@gmail.com>
2026-08-14 16:00:33 -0700

    implement responsive navbar with toggle functionality for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  const close = () => setOpen(false);
+
   return (
-    <nav className="navbar">
-      <Link to="/" aria-label="Mojo Coffee home">
+    <nav className={`navbar${open ? " navbar--open" : ""}`}>
+      <Link to="/" aria-label="Mojo Coffee home" onClick={close}>
         <img
           className="wordmark"
           src={`${import.meta.env.BASE_URL}mojo-coffee.png`}
@@ -13,23 +17,41 @@ function Navbar() {
         />
       </Link>
 
+      <button
+        type="button"
+        className="navbar__toggle"
+        aria-expanded={open}
+        aria-label={open ? "Close menu" : "Open menu"}
+        onClick={() => setOpen(!open)}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
+
       <ul className="navbar__links">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={close}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/products">Products</Link>
+          <Link to="/products" onClick={close}>
+            Products
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" onClick={close}>
+            Contact
+          </Link>
         </li>
       </ul>
 
       <div className="navbar__actions">
-        <Link to="/order" className="btn btn--sage">
+        <Link to="/order" className="btn btn--sage" onClick={close}>
           Order Ahead
         </Link>
-        <Link to="/login" className="btn btn--sage">
+        <Link to="/login" className="btn btn--sage" onClick={close}>
           Login
         </Link>
       </div>
